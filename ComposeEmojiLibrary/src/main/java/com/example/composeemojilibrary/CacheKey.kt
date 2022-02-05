c25f52b29471c600634cc7434ea4ae173ff233c5
anorld-droid <mulindipatrice00@gmail.com>
2022-02-05 06:07:58 +0300

    New class for caching.

---
+++ ComposeEmojiLibrary/src/main/java/com/example/composeemojilibrary/CacheKey.kt
@@ -0,0 +1,12 @@
+package com.example.composeemojilibrary
+
+class CacheKey(private val x: Int, private val y: Int) {
+    override fun equals(other: Any?): Boolean {
+        return (other is CacheKey
+                && x == other.x && y == other.y)
+    }
+
+    override fun hashCode(): Int {
+        return x shl 16 xor y
+    }
+}
