df23590b2d2352a177e198c107482a804a3d1a07
anorld-droid <mulindipatrice00@gmail.com>
2022-02-18 14:48:12 +0300

    Changed the category implementation classes to java.

---
+++ Ios-Emoji/src/main/java/com/example/ios_emoji/category/SmileysAndPeopleCategory.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 - Niklas Baudy, Ruben Gees, Mario Đanić and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.ios_emoji.category;
+
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+
+import com.example.composeemojilibrary.EmojiCategory;
+import com.example.ios_emoji.IosEmoji;
+import com.example.ios_emoji.R;
+
+@SuppressWarnings("PMD.MethodReturnsInternalArray") public final class SmileysAndPeopleCategory implements EmojiCategory {
+  private static final IosEmoji[] EMOJIS = CategoryUtils.INSTANCE.concatAll(SmileysAndPeopleCategoryChunk0.INSTANCE.get(), SmileysAndPeopleCategoryChunk1.get());
+
+  @Override @NonNull public IosEmoji[] getEmojis() {
+    return EMOJIS;
+  }
+
+  @Override @DrawableRes public int getIcon() {
+    return R.drawable.emoji_ios_category_smileysandpeople;
+  }
+
+  @Override @StringRes public int getCategoryName() {
+    return R.string.emoji_ios_category_smileysandpeople;
+  }
+}
